175b742880da3b16201a8f45638ce05fae60a496
Wesley Kolar <wrkolar@gmail.com>
2021-02-13 20:13:34 -0600

    changes

--- pooltable.py
+++ pooltable.py
@@ -10,7 +10,7 @@ class Formatter():
         time = end - start 
         sec = time.total_seconds()
         hours = round(sec / (60 * 60))
-        minutes = sec % (60 * 60)                          #timer formatting
+        minutes = sec % (60 * 60)                          
         minutes = round(minutes / 60)
         better_timer = f"{hours} hrs : {minutes} min"
         return better_timer
@@ -20,7 +20,7 @@ class Formatter():
         minute = time.minute
         if minute < 10:
             minute = f"0{minute}"
-            self.clock = "AM"                               #more formatting
+            self.clock = "AM"                               
         if hour > 12:
             hour -= 12
             self.clock = "PM"
@@ -28,7 +28,7 @@ class Formatter():
         return better_time
 
 formatter = Formatter()
-# table class what attributes should be assigned to table
+
 class Table:
     def __init__(self, number):
         self.number = number
@@ -37,19 +37,19 @@ class Table:
         self.end_time = ""
         self.time_played = ""
         self.current_time = ""
-# checkout function
+
     def checkout(self):
         if self.occupied == True:
-            print("")             #if/else preventing user from selecting a table that is already checked out
+            print("")             
             input(f" Table {self.number} is currently occupied. Press enter to return to main.")
         else:
-            self.occupied = True #make it true
-            self.start_time = datetime.now() #start time
-            self.end_time = datetime.now() #end time
-            self.time_played = self.end_time - self.start_time #time elapsed
+            self.occupied = True 
+            self.start_time = datetime.now() 
+            self.end_time = datetime.now() 
+            self.time_played = self.end_time - self.start_time 
 
-    def checkin(self): #check in function
-        if self.occupied == False: #if user hits a table that is already turned in, it doesnt break to program
+    def checkin(self): 
+        if self.occupied == False: 
             print("")
             input("This table is currently unoccupied, please press enter.")
             return False
@@ -57,16 +57,16 @@ class Table:
             with open("log.txt", "a") as file:
                 file.write(f"Table number:{self.number} \nStart time and Date: {self.start_time} \nEnd time and date: {self.end_time} \nTime Played: {self.time_played}")
                 file.write("\n")
-            self.occupied = False # if its an occupied table that is correct because its being turned in
-            self.end_time = datetime.now() #end time
-            self.time_played = self.end_time - self.start_time #elapsed time
+            self.occupied = False 
+            self.end_time = datetime.now() 
+            self.time_played = self.end_time - self.start_time 
             return True
             
 class TableManager():
     def __init__(self): 
         self
 
-    def show_menu(self): #function to call menu screen
+    def show_menu(self): 
         print("")
         print("++++++++++ University Pool Tables ++++++++++")
         print("---------------------------------------------")
@@ -76,48 +76,48 @@ class TableManager():
         print("                Quit? Enter q ")
         print("---------------------------------------------")
 
-    def show_tables(self): #function that shows tables when use enters 3
+    def show_tables(self): 
         current_time = datetime.now()
-        print("") #looks nice
-        for table in tables: #looks nice
-            if table.occupied == True: #if someone is using it that means its (true)ly occupied, status shows X
-                status = "X"
+        print("") 
+        for table in tables: 
+            if table.occupied == True: 
+                status = "XXXXXXXXXXXXXX"
             else:
-                status = "Available Now" #otherwise, shows available
-            if table.start_time != "": #!= (if start time is not "null") the time checked out and whatnot
+                status = "Available Now" 
+            if table.start_time != "": 
                 better_clock = formatter.clock_format(table.start_time,) 
                 elapsed_time = formatter.timer_format(
                     current_time, table.start_time)
-                print(f"Table-{table.number} - {status} -  Start: {better_clock} - Play time: {elapsed_time}") #display when user enters 3
+                print(f"Table-{table.number} - {status} -  Start: {better_clock} - Play time: {elapsed_time}") 
             else:
-                print(f"Table-{table.number} - {status}") #if it is null show available status
+                print(f"Table-{table.number} - {status}") 
     
-    def choose_table(self, user_input): #choose table function
-        while True: #while loop so it prompts over
-            try: #try thing we learned the other day
+    def choose_table(self, user_input): 
+        while True: 
+            try: 
                 if user_input == "1":
-                    choice = int(input("Enter table number to check out: ")) - 1 #because we have int we have try/except
+                    choice = int(input("Enter table number to check out: ")) - 1 
 
                 else:
-                    choice = int(input("Enter table number to return: ")) - 1 #same as above
+                    choice = int(input("Enter table number to return: ")) - 1 
 
-                table = tables[choice] #table equals table-int user inputs in easily callable variable
-                return table #calls that variable
-            except ValueError: #exception to try 
+                table = tables[choice] 
+                return table 
+            except ValueError: 
                 print("")
-                print("Please enter a valid table number:") #nice looking except statement
+                print("Please enter a valid table number:") 
                 print("")
 
-    def choices(self, user_input): #function for choices
+    def choices(self, user_input): 
         if user_input == "1":
             print("")
-            confirmation = input("New Table? Enter y/n: ") #confirms
+            confirmation = input("New Table? Enter y/n: ") 
             if confirmation == "n":
                 print("")
             elif confirmation == "y":
-                self.show_tables() #calls show table availability function
+                self.show_tables() 
                 table = self.choose_table(user_input)
-                table.checkout() #calls checkout function
+                table.checkout() 
                 self.show_tables() 
                 print("")
                 print(f"Table {table.number} has been checked out at: {formatter.clock_format(table.start_time)}")
@@ -132,17 +132,17 @@ class TableManager():
             if confirmation == "n":
                 print("")
             elif confirmation == "y":
-                self.show_tables() #show table function
-                all_available = True #if all are available
+                self.show_tables() 
+                all_available = True 
                 for table in tables:
-                    if table.occupied == True: #tells it how to check if all tables are available
+                    if table.occupied == True: 
                         all_available = False
-                if all_available == True: #if its true this isnt what you want
+                if all_available == True: 
                     print("")
                     input("All Tables are available.  Press enter to continue: ")
                 else:
                     table = self.choose_table(user_input)
-                    status = table.checkin() #calls check in function
+                    status = table.checkin() 
                     if status == True: 
                         table.start_time = ""
                         self.show_tables()
@@ -151,15 +151,15 @@ class TableManager():
                 print("")
                 print("You did not enter a valid response.")
         elif user_input == "3":
-            self.show_tables() #calls table when user selects option 3
+            self.show_tables() 
 
 manager = TableManager() 
-tables = [] #nice array for the tables
+tables = []
 for i in range(1, 13):
     table = Table(i)
     tables.append(table)
 user_input = ""
-while user_input != "q": #loops it to show menu and choice anytime the answer is not q...so even if its not an option, it wont quit unless you press q
+while user_input != "q": 
     manager.show_menu()
     user_input = input("Please enter a choice from the menu above: ")
     manager.choices(user_input)
